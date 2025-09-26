5b0a7f2693988af3671fd8d8af7a54c387accbfc
IT24100948 <IT24100948@my.sliit.lk>
2025-09-26 12:02:59 +0530

    IT24100948Lab_6

---
+++ Lab Sheet 06/IT24100948Lab_6.R
@@ -0,0 +1,14 @@
+setwd("C:\\Users\\it24101105\\Desktop\\IT24100948")
+#Q1 X follows the Binomial Distribution
+#Q2 Probability that at least 47 students passed
+
+#Binomial distributio: n = 50, p = 0.85
+1 - pbinom(46, 50, 0.85)
+
+#Q1 X = Number of customer calls recieved per hour
+#Q2 X follows a poisson distribution:
+#Q3
+#poisson distribution: lambda = 12
+dpois(15,12)
+
+
